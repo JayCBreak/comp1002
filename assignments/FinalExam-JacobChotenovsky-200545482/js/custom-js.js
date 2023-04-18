944597a3b4f4bb741df10272c3195badd478d1a5
Jacob Chotenovsky <github@jacobchotenovsky.com>
2023-04-18 11:43:54 -0400

    Final Exam

---
+++ assignments/FinalExam-JacobChotenovsky-200545482/js/custom-js.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    // Call Header and Footer Files
+    $(function(){
+        $("#header").load("header.html");
+        $("#footer").load("footer.html");
+    });
+});
